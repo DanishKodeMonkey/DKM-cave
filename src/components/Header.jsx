98e2c304c5cfcff1be49ce5ce52242f3991bb5a0
danishkodemonkey <humberjini@hotmail.com>
2024-07-17 15:37:04 +0200

    Feat: Header component initial pass

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className='flex flex-col justify-center align-center text-center'>
+            <h1 className='text-4xl'>danishKodeMonkey</h1>
+            <h2 className='text-2xl'>Web wrench jockey </h2>
+            <h3 className='text-1xl'>Homepage of Daniel Ljung Runge</h3>
+        </div>
+    );
+}
+
+export default Header;
