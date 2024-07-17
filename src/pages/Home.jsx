be3cb33db1dfef9894b3159d2d4340da7d226fea
danishkodemonkey <humberjini@hotmail.com>
2024-07-17 15:48:12 +0200

    feat: home page initial pass

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className='text-center'>
+            <h1 className='text-5xl'>Welcome!</h1>
+            <h2 className='text-2xl'>
+                This is the website of Daniel Ljung Runge
+            </h2>
+            <p>
+                The danish code monkey with a passion for web development with
+                flair!
+            </p>
+            <p>Have a look around, and please do enjoy your stay!</p>
+        </div>
+    );
+}
+
+export default Home;
