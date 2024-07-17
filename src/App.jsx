77f4fdd018ed7bc01becd25dc9b5a63172ff49c9
danishkodemonkey <humberjini@hotmail.com>
2024-07-17 15:48:23 +0200

    feat: implement home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
     return (
-        <div className='flex flex-col h-screen bg-slate-300'>
-            <header className='bg-blue-500 flex-2'>
+        <div className='flex flex-col h-screen bg-slate-300 gap-2'>
+            <header className='bg-blue-500 flex-2 py-4'>
                 <Header />
             </header>
-            <main className='bg-green-500 flex-1'>
-                <h3 className='text-xl'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Possimus enim velit praesentium nemo odio adipisci itaque,
-                    esse molestias saepe ipsam at nulla. Asperiores sed officia
-                    assumenda dicta rerum incidunt consectetur.
-                </h3>
+            <main className='bg-green-500 flex-1 py-4 px-5'>
+                <Home />
             </main>
             <footer className='bg-red-500 flex-2'>
                 <Footer />
