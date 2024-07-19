56d7de2e1fccc29b83f8ea6a415e2aab14b5cfb8
danishkodemonkey <humberjini@hotmail.com>
2024-07-19 14:47:59 +0200

    feat: navbar to header with links to pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 function Header() {
     return (
         <div className='flex flex-col justify-center align-center text-center'>
-            <h1 className='text-4xl'>danishKodeMonkey</h1>
-            <h2 className='text-2xl'>Web wrench jockey </h2>
-            <h3 className='text-1xl'>Homepage of Daniel Ljung Runge</h3>
+            <div>
+                <h1 className='text-4xl'>danishKodeMonkey</h1>
+                <h2 className='text-2xl'>Web wrench jockey </h2>
+                <h3 className='text-1xl'>Homepage of Daniel Ljung Runge</h3>
+            </div>
+            <div>
+                <Navbar />
+            </div>
         </div>
     );
 }
