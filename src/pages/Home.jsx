8d58ea181434b3dac5fa6a3a98f746b064494af1
danishkodemonkey <humberjini@hotmail.com>
2024-07-24 08:42:19 +0200

    feat: custom text animation on load

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 function Home() {
     return (
         <div className='text-center'>
-            <h1 className='text-5xl'>Welcome!</h1>
-            <h2 className='text-2xl'>
+            <h1 className='text-5xl opacity-0 animate-text-slideIn-top-1'>
+                Welcome!
+            </h1>
+            <h2 className='text-2xl opacity-0 animate-text-slideIn-top-2'>
                 This is the website of Daniel Ljung Runge
             </h2>
-            <p>
+            <p className='opacity-0 animate-text-slideIn-top-3'>
                 The danish code monkey with a passion for web development with
                 flair!
             </p>
-            <p>Have a look around, and please do enjoy your stay!</p>
+            <p className='opacity-0 animate-text-slideIn-top-4'>
+                Have a look around, and please do enjoy your stay!
+            </p>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                textSlide: {
+                    '0%': {
+                        transform: 'translateY(-100%)',
+                        opacity: '0',
+                    },
+                    '100%': {
+                        transform: 'translateY(0)',
+                        opacity: '1',
+                    },
+                },
+            },
+            animation: {
+                'text-slideIn-top-1': 'textSlide 1s forwards',
+                'text-slideIn-top-2': 'textSlide 1s forwards 200ms',
+                'text-slideIn-top-3': 'textSlide 1s forwards 400ms',
+                'text-slideIn-top-4': 'textSlide 1s forwards 600ms',
+                'text-slideIn-top-5': 'textSlide 1s forwards 800ms',
+            },
+        },
     },
     plugins: [],
 };
